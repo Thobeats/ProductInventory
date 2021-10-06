5ff339cb2e2628836802972387b58e3e7a23b64c
firstlincoln <firstlincoln@macs-MacBook-Pro.local>
2021-10-06 18:55:46 +0100

    update

--- classes/Database.php
+++ classes/Database.php
@@ -3,17 +3,17 @@
 namespace App;
 
 class Database{
-    public $host;
-    public $username;
-    public $password;
-    public $database;
+    public $host = "eu-cdbr-west-01.cleardb.com";
+    public $username = "bd1a69e060a62b";
+    public $password = "73b0f3da";
+    public $database = "heroku_77923308437b88f";
 
-    public function __construct($host, $username, $password, $database){
-        $this->host = $host;
-        $this->username = $username;
-        $this->password = $password;
-        $this->database = $database;
-    }
+    // public function __construct($host, $username, $password, $database){
+    //     $this->host = $host;
+    //     $this->username = $username;
+    //     $this->password = $password;
+    //     $this->database = $database;
+    // }
 
     public function connect(){
        $con = mysqli_connect($this->host, $this->username, $this->password, $this->database);
--- delete_products.php
+++ delete_products.php
@@ -8,7 +8,7 @@ require_once realpath("vendor/autoload.php");
 $idData = $_POST['delete'];
 $values = [];
 $product_control = new Product_control();
-$database = new Database("eu-cdbr-west-01.cleardb.com","bd1a69e060a62b","73b0f3da","heroku_77923308437b88f");
+$database = new Database();
 $link = $database->connect();
 
 foreach($idData as $id){
